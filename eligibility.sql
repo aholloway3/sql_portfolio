1a64fb1d80c73d3212ce5bd451976e363fe16e26
Alan Holloway <50306706+aholloway3@users.noreply.github.com>
2026-04-23 00:12:07 -0500

    Create eligibility.sql

---
+++ eligibility.sql
@@ -0,0 +1,15 @@
+-- Find eligible patients
+SELECT
+p.patient_id,
+p.patient_name,
+p.diagnosis_code,
+p.age,
+p.last_admission_date
+FROM patients p
+INNER JOIN eligibility_rules r ON p.diagnosis_code = r.diagnosis_code
+WHERE p.age BETWEEN r.min_age AND r.max_age
+	AND (r.requires_recent_admission = 0 OR
+	(r. requires_recent_admission = 1 AND 
+	p.last_admission_date >= date('now', '-3months')))
+	AND p.is_active =1;
+	
